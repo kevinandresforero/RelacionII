297758426ed7885a812074a4c46a6c0df81d2f5d
Kevin Andres Forero Guaitero <42241664+kevinandresforero@users.noreply.github.com>
2020-02-20 00:27:34 -0500

    No crea Relaciones

--- Db.py
+++ Db.py
@@ -4,34 +4,37 @@ conexion = pymysql.connect(host="localhost",
                            user="root",
                            passwd="261120",
                            database="Prueba")
-
-Estudiantes = """CREATE TABLE Students(
+cursor = conexion.cursor()
+try:
+    Estudiantes = """CREATE TABLE Students(
     IdE int NOT NULL,
     NomE varchar(15) NOT NULL,
     PRIMARY KEY(IdE)
-) ;
-"""
+    ) ;
+    """
 
-Materias = """CREATE TABLE Materias(
-    idM int NOT NULL,
-    NomM varchar(15) NOT NULL,
-    PRIMARY KEY(idM)
-) ;
-"""
+    Materias = """CREATE TABLE Materias(
+        idM int NOT NULL,
+        NomM varchar(15) NOT NULL,
+        PRIMARY KEY(idM)
+    ) ;
+    """
+    cursor.execute(Estudiantes)
+    cursor.execute(Materias)
+    conexion.commit()
+except:
+    print("Ya estan creadas las tablas Estudiantes y Materias")
 
-Relaciones = """CREATE TABLE Relaciones(idE_foran int, idM_foran int,
-FOREING KEY (idA_foran) REFERENCES Estudiantes(idE),
-FOREING KEY (idM_foran) REFERENCES Materias(idM)
-);
-"""
-
-cursor = conexion.cursor()
-cursor.execute(Relaciones)
-cursor.execute(Estudiantes)
-cursor.execute(Materias)
+try:
+    Relaciones = """CREATE TABLE Relaciones(idE_foran int, idM_foran int,
+    FOREING KEY (idA_foran) REFERENCES Estudiantes(idE),
+    FOREING KEY (idM_foran) REFERENCES Materias(idM)
+    );
+    """
+    cursor.execute(Relaciones)
+    conexion.commit()
+except:
+    print("No se pudo crear la tabla relaciones ... Paila Socio")
 cursor.close()
-
-
-conexion.commit()
 conexion.close()
 
